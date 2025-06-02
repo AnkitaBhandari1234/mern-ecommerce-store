6dd3f0fc011dedaa0a1cc83c66a0998d0a3bd7db
AnkitaBhandari1234 <ankitabhandari376@gmail.com>
2025-06-02 12:22:29 +0545

    products

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,35 +1,42 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from './Title';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
+  // getting products data using context API
+  const { products } = useContext(ShopContext);
 
+  // to add recently added 10 products
+  const [latestProducts, setLatestProducts] = useState([]);
 
-    // getting products data using context API
-const {products}=useContext(ShopContext);
-
-// to add recently added 10 products
-const[latestProducts,setLatestProducts]=useState([]);
-
-// to load the 10 prodcuts from products from above products state
-useEffect(()=>{
-  setLatestProducts(products.slice(0,10))
-},[])
+  // to load the 10 prodcuts from products from above products state
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 11));
+  }, []);
 
   return (
-    <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Title text1={"Latest"} text2={"Collections"}/>
-            <p className='w-3/4 m-auto text-xs sm:text-base text-gray-600'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
-            </p>
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"Latest"} text2={"Collections"} />
+        <p className="w-3/4 m-auto text-xs sm:text-base text-gray-600">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the.
+        </p>
+      </div>
 
-
-        </div>
-
-
+      {/* Rendering product */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {
+          latestProducts.map((val,i)=>{
+            return(
+              <ProductItem key={i} id={val._id} image={val.image} name={val.name} price={val.price}/>
+            )
+          })
+        }
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LatestCollection
+export default LatestCollection;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,13 @@ import { IoBagOutline } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoMdClose } from "react-icons/io";
 
-
 // useLocation means home page '/' this
 
 const Navbar = () => {
   // for apperance of hr bar below the nav list item when clicked
   const [activeIndex, setActiveIndex] = useState(null);
+
+  const location = useLocation();
 
   // set the hr in home page
   useEffect(() => {
@@ -21,7 +22,7 @@ const Navbar = () => {
   }, [location.pathname]);
 
   // to appear the hamburger content
-  const [visible,setVisible]=useState(false);
+  const [visible, setVisible] = useState(false);
 
   const navitem = [
     {
@@ -48,7 +49,9 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5   font-medium ">
       {/* for image */}
-      <img src={assets.logo} alt="logo" className="w-36"></img>
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="w-36"></img>
+      </Link>
 
       {/* for nav list item */}
       <ul className=" hidden sm:flex items-center text-sm text-gray-700 gap-5 ">
@@ -96,34 +99,54 @@ const Navbar = () => {
         </Link>
         {/* hamburger menu */}
 
-           {/* here sm:hidden means hamburger icon is hidden when screen is larger */}
-        <RxHamburgerMenu className="text-2xl cursor-pointer sm:hidden " onClick={()=>{setVisible(true)}} />
-          {
-            // here sm:hidden means ham burger content is hidden when screen is large
-            visible?<div className="absolute top-0 left-0 w-full h-screen sm:hidden  ">
+        {/* here sm:hidden means hamburger icon is hidden when screen is larger */}
+        <RxHamburgerMenu
+          className="text-2xl cursor-pointer sm:hidden "
+          onClick={() => {
+            setVisible(true);
+          }}
+        />
+        {
+          // here sm:hidden means ham burger content is hidden when screen is large
+          visible ? (
+            <div className="absolute top-0 left-0 w-full h-screen sm:hidden  ">
               <div className=" px-4 py-8 flex flex-col gap-5 w-7/12 h-screen bg-white  ">
                 <div className="flex items-center justify-between">
-
-              <img src={assets.logo} alt="" className="w-24"/>
-              <IoMdClose className="text-xl cursor-pointer" onClick={()=>{setVisible(false)}} />
+                  <img src={assets.logo} alt="" className="w-24" />
+                  <IoMdClose
+                    className="text-xl cursor-pointer"
+                    onClick={() => {
+                      setVisible(false);
+                    }}
+                  />
                 </div>
 
                 {/* link list */}
                 <div className="flex flex-col uppercase font-sans font-medium tracking-wide gap-3">
-
-                <Link onClick={()=>{setVisible(false)}} to='/' className="pl-4 hover:font-bold duration-300">Home</Link>
-                <Link onClick={()=>{setVisible(false)}} to='/collection' className="pl-4 hover:font-bold duration-300">Collection</Link>
-                <Link onClick={()=>{setVisible(false)}} to='/about' className="pl-4 hover:font-bold duration-300">About</Link>
-                <Link onClick={()=>{setVisible(false)}} to='/contact' className="pl-4 hover:font-bold duration-300">Contact</Link>
+                  {navitem.map((val, i) => {
+                    return (
+                      <Link
+                        key={i}
+                        onClick={() => {
+                          setVisible(false);
+                          setActiveIndex(i);
+                        }}
+                        to={val.path}
+                        className={`pl-4 py-2 rounded-md duration-300 ${
+                          activeIndex === i
+                            ? "bg-gray-700  text-white"
+                            : "hover:font-bold"
+                        }`}
+                      >
+                        {val.title}
+                      </Link>
+                    );
+                  })}
                 </div>
-
-
-              </div> 
-            
-            </div>:null
-          }
-
-
+              </div>
+            </div>
+          ) : null
+        }
       </div>
     </div>
   );
